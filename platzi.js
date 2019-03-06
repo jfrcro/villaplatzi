764f3f854848b7ecf83edc5cd32b86a0878f18ec
Jeffer C <jffrcro@gmail.com>
2019-03-06 16:50:55 -0500

    he agregado un ciclo for para iterar 10 veces la impresion separados por coma, se imprime

--- platzi.js
+++ platzi.js
@@ -1,7 +1,12 @@
-var z = aleatorio(10, 20);
-document.write(z);
+var z;
 
-function aleatorio (min, max);
+for (var i=0;i<10;i++)
+{
+  var z = aleatorio(10, 20);
+  document.write(z + ", ");
+}
+
+function aleatorio(min, max)
 {
   var resultado;
   resultado = Math.floor(Math.random() * (max - min + 1)) + min;

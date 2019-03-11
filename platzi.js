ad5b54033492da1e7d3d94e0fcf65ac84f1b92b1
Jeffer C <jffrcro@gmail.com>
2019-03-11 09:05:58 -0500

    primera aparicion de vaca,cerdo,pollo. dibujos sin orden

--- platzi.js
+++ platzi.js
@@ -1,15 +1,37 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
-var mapa = "tile.png";
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+  fondo.src = "tile.png";
+  fondo.addEventListener("load", dibujarFondo);
+var vaca = new Image();
+  vaca.src = "vaca.png";
+  vaca.addEventListener("load", dibujarVacas);
+var cerdo = new Image();
+  cerdo.src = "cerdo.png";
+  cerdo.addEventListener("load", dibujarCerdos);
+var pollo = new Image();
+  pollo.src = "pollo.png";
+  pollo.addEventListener("load", dibujarPollos);
 
-function dibujar()
+function dibujarFondo()
 {
-  papel.drawImage(imagen, 0, 0);
+  papel.drawImage(fondo, 0, 0);
 }
+function dibujarVacas()
+{
+  papel.drawImage(vaca, 10, 10);
+}
+function dibujarCerdos()
+{
+  papel.drawImage(cerdo, 10, 300);
+}
+function dibujarPollos()
+{
+  papel.drawImage(pollo, 300, 150);
+}
+
+
 
 function aleatorio(min, max)
 {

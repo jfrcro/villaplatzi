bb53e1e604dd1c7527f0a2bd604fe32a2576c58d
Jeffer C <jffrcro@gmail.com>
2019-03-11 08:40:40 -0500

    primera aparicion del fondo en canvas desde js (objeto+ruta+escuchador+funcion dibujar)

--- platzi.js
+++ platzi.js
@@ -1,6 +1,16 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
+var mapa = "tile.png";
+var imagen = new Image();
+imagen.src = mapa;
+imagen.addEventListener("load", dibujar);
+
+function dibujar()
+{
+  papel.drawImage(imagen, 0, 0);
+}
+
 function aleatorio(min, max)
 {
   var resultado;

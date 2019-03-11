90a427f87e7074809cca18f0b134daad11a9af17
Jeffer C <jffrcro@gmail.com>
2019-03-11 18:07:07 -0500

    con pollos

--- platzi.js
+++ platzi.js
@@ -5,11 +5,18 @@ var fondo = {
   url: "tile.png",
   cargaOK: false
 }
-
 var vaca = {
   url: "vaca.png",
   cargaOK: false
-}
+};
+
+var pollo = {
+  url: "pollo.png",
+  cargaOK: false
+};
+
+
+var cantidad = aleatorio(1, 10);
 
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
@@ -19,43 +26,61 @@ vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
 
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
 function cargarFondo()
 {
   fondo.cargaOK = true;
   dibujar();
 }
-
 function cargarVacas()
 {
   vaca.cargaOK = true;
   dibujar();
 }
 
-var cantidad = aleatorio(1, 10);
+function cargarPollos()
+{
+  pollo.cargaOK = true;
+  dibujar();
+}
 
 function dibujar()
 {
-  if (fondo.cargaOK)
-    {
-      papel.drawImage(fondo.imagen, 0, 0);
-    }
-  if (vaca.cargaOK)
-    {
-      console.log(cantidad);
-      for(var v=0; v < cantidad; v++)
+  if(fondo.cargaOK)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if(vaca.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
     {
       var x = aleatorio(0, 7);
-      var y = aleatorio(0, 7);
-      var x = x * 60
-      var y = y * 60
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
       papel.drawImage(vaca.imagen, x, y);
     }
   }
+  if(pollo.cargaOK)
+  {
+    console.log(cantidad);
+    for(var v=0; v < cantidad; v++)
+    {
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(pollo.imagen, x, y);
+    }
+  }
 }
-
-function aleatorio(min, max)
+function aleatorio(min, maxi)
 {
   var resultado;
-  resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
   return resultado;
 }

a9ad9dda46f8b85fa64b7d564df64680dad13f71
Jeffer C <jffrcro@gmail.com>
2019-03-11 17:39:38 -0500

    se integra codigo para dibujar un cerdo y generar el movimiento unicamente

--- platzi.js
+++ platzi.js
@@ -1,55 +1,9 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 
-var fondo = {
-  url: "tile.png",
+var cerdo = {
+  url: "cerdo.png",
   cargaOK: false
-}
-
-var vaca = {
-  url: "vaca.png",
-  cargaOK: false
-}
-
-fondo.imagen = new Image();
-fondo.imagen.src = fondo.url;
-fondo.imagen.addEventListener("load", cargarFondo);
-
-vaca.imagen = new Image();
-vaca.imagen.src = vaca.url;
-vaca.imagen.addEventListener("load", cargarVacas);
-
-function cargarFondo()
-{
-  fondo.cargaOK = true;
-  dibujar();
-}
-
-function cargarVacas()
-{
-  vaca.cargaOK = true;
-  dibujar();
-}
-
-var cantidad = aleatorio(1, 9);
-
-function dibujar()
-{
-  if (fondo.cargaOK)
-  {
-    papel.drawImage(fondo.imagen, 0, 0);
-  }
-  if (vaca.cargaOK)
-  {
-
-    console.log(cantidad);
-    for(var v=0; v < cantidad; v++)
-    {
-    var x = aleatorio(0, 420);
-    var y = aleatorio(0, 420);
-    papel.drawImage(vaca.imagen, x, y);
-    }
-  }
 }
 
 function aleatorio(min, max)
@@ -57,4 +11,73 @@ function aleatorio(min, max)
   var resultado;
   resultado = Math.floor(Math.random() * (max - min + 1)) + min;
   return resultado;
+}
+var cantidad = aleatorio(1, 10);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+function cargarCerdo()
+{
+  cerdo.cargaOK = true;
+  dibujar();
+}
+
+var x;
+var y;
+
+function dibujar()
+{
+  if (cerdo.cargaOK)
+  {
+    // for(var v=0; v < cantidad; v++)
+    // {
+      x = aleatorio(0, 7);
+      y = aleatorio(0, 7);
+      x = x * 60;
+      y = y * 60;
+      papel.drawImage(cerdo.imagen, x, y);
+    // }
+  }
+}
+
+var teclasDibujar =
+{
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+document.addEventListener("keydown", dibujarTeclado);
+
+function dibujarTeclado(eventoTeclado)
+{
+  var colorLinea = "red";
+  var movimiento = 5;
+
+  switch (eventoTeclado.keyCode)
+    {
+      case teclasDibujar.LEFT:
+        papel.drawImage(cerdo.imagen, x - movimiento, y);
+        x = x - movimiento;
+          break;
+      case teclasDibujar.UP:
+        papel.drawImage(cerdo.imagen, x, y - movimiento);
+        y = y - movimiento;
+          break;
+      case teclasDibujar.RIGHT:
+        papel.drawImage(cerdo.imagen, x + movimiento, y);
+        x = x + movimiento;
+          break;
+      case teclasDibujar.DOWN:
+        papel.drawImage(cerdo.imagen, x, y + movimiento)
+      y = y + movimiento;
+          break;
+      default:
+        console.log("Usa las teclas: ← ↑ → ↓ ");
+          break;
+
+    }
 }
